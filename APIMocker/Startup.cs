7a8f1f6a7affa119f6b4a594d6a4695f8f3d5467
Mohsen <mkokabi@yahoo.com>
2020-08-31 22:41:36 +1000

    Adding post from client side

--- APIMocker/Startup.cs
+++ APIMocker/Startup.cs
@@ -51,7 +51,7 @@ namespace APIMocker
         var response = context.HttpContext.Response;
         var match = APIDetailOptions.FirstOrDefault(opt =>
                   path.Equals(opt.Path, StringComparison.OrdinalIgnoreCase) &&
-                  QueryStringChecker.Match(opt.QueryString, queryString) && 
+                  QueryStringChecker.Match(opt.QueryString, queryString) &&
                   string.Equals(opt.Method, method, StringComparison.InvariantCultureIgnoreCase));
         if (match != null)
         {
@@ -85,14 +85,37 @@ namespace APIMocker
     {
       response.ContentType = "text/html";
       var sb = new StringBuilder("<h2>MockApi</h2>");
+      var url = $"{(request.IsHttps ? "https" : "http")}://{request.Host}";
+      sb.Append($@"<script>
+      function callPost(path, queryString, method) {{
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {{
+          if (this.readyState == 4 && this.status == 200) {{
+              alert(xhr.responseText);
+            }}
+        }};
+        var yourUrl='{url}' + path + queryString;
+        xhr.open(method, yourUrl, true);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.send();
+      }}
+</script>");
       sb.Append("<p style='color: green'>running</p>");
       sb.Append("<br><p>registered API mocks:</p>");
       sb.Append("<style>table {border-collapse: collapse;} td, th { border: 1px solid black; }</style>");
       sb.Append("<table><tr><th>Path</th><th>Query string</th><th>method</th><th></th></tr>");
-      APIDetailOptions.ForEach(opt => 
-        sb.Append($@"<tr><td>{opt.Path}</td><td>{opt.QueryString}</td><td>{opt.Method.ToUpper()}</td>
-        <td><a target='_blank' href='{
-          (request.IsHttps ? "https" : "http")}://{request.Host}{opt.Path}{opt.QueryString}'>test</a></td></tr>"));
+      APIDetailOptions.ForEach(opt => {
+        sb.Append($"<tr><td>{opt.Path}</td><td>{opt.QueryString}</td><td>{opt.Method.ToUpper()}</td><td>");
+        if (opt.Method == "GET")
+        {
+          sb.Append($"<a target='_blank' href='{url}{opt.Path}{opt.QueryString}'>test</a>");
+        }
+        else
+        {
+          sb.Append($"<button onclick='callPost(\"{opt.Path}\",\"{opt.QueryString}\",\"{opt.Method}\")'>test</button>");
+        }
+        sb.Append("</td></tr>");
+      });
       sb.Append("</table>");
       await response.BodyWriter.WriteAsync(Encoding.UTF8.GetBytes(sb.ToString()));
     }
