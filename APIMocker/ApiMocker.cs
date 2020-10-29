d0862852456c7ff2f96a78420b283f7f35cdfe7e
Mohsen Kokabi <mohsen.kokabi@ugllimited.com>
2020-10-29 18:01:06 +1100

    Adding minvalue and maxvalue to random

--- APIMocker/ApiMocker.cs
+++ APIMocker/ApiMocker.cs
@@ -9,13 +9,25 @@ namespace APIMocker
     public static string Replace(string responseBodyString, List<Replace> replaces)
     {
       const string randomRegex = @"\{\s*Random\s*\(\s*(?:[0-9]*|[0-9]\s*\,\s*[0-9]*)\s*\)\s*}";
+      const string randomHeadRegex = @"\{\s*Random\s*\(";
+      char[] randomTailChars = {' ', ')', '}'};
       const RegexOptions regexOptions = RegexOptions.IgnoreCase | RegexOptions.IgnorePatternWhitespace;
+      var random = new Random();
       foreach (var replace in replaces)
       {
         var replaceWith = replace.Replacement;
         if (Regex.Match(replace.Replacement, randomRegex, regexOptions).Success)
         {
-          replaceWith = new Random().Next().ToString();
+          var numbersPart = Regex.Replace(replace.Replacement, randomHeadRegex, "").TrimEnd(randomTailChars);
+          var numbers = numbersPart.Split(',', StringSplitOptions.RemoveEmptyEntries);
+          
+          replaceWith = (numbers.Length == 2 && int.TryParse(numbers[0], out int minValue)
+          && int.TryParse(numbers[1], out int maxValue) && (minValue < maxValue)) ? 
+            random.Next(minValue, maxValue).ToString() :
+            (
+              (numbers.Length == 1 && int.TryParse(numbers[0], out maxValue)) ? 
+              random.Next(maxValue).ToString() : random.Next().ToString()
+            );
         }
         responseBodyString = Regex.Replace(responseBodyString, replace.Pattern, replaceWith);
       }
